f3bbde842883640a05a5298e2593b8463f87efd6
FredeH <frede.hansen09@gmail.com>
2024-01-15 17:23:39 +0100

    Initial commit

---
+++ PDM-Checker-Add-in/Task/TaskLaunch.cs
@@ -0,0 +1,13 @@
+﻿using EPDM.Interop.epdm;
+
+namespace PDM_Checker_Add_in
+{
+    public partial class AddIn
+    {
+
+        public void TaskLaunch(ref EdmCmd cmdData, ref EdmCmdData[] ppoData)
+        {
+
+        }
+    }
+}
---
+++ PDM-Checker-Add-in/Task/TaskSetup.cs
@@ -0,0 +1,13 @@
+﻿using EPDM.Interop.epdm;
+
+namespace PDM_Checker_Add_in
+{
+    public partial class AddIn
+    {
+
+        public void TaskSetup(ref EdmCmd cmdData, ref EdmCmdData[] ppoData)
+        {
+
+        }
+    }
+}
